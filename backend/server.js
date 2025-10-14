4ea97a97cce4561371cf183cd7642be8d9fc8117
reyhane9015 <rafati.reyhaneh@yahoo.com>
2025-10-14 18:22:41 +0330

    fixed

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "*",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
