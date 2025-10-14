40924403b1e708d45dfdef9a6d729f675ec2d860
Reyhaneh <48958829+reyhane9015@users.noreply.github.com>
2025-10-14 18:47:05 +0330

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://cv-builder-frontend-qcrt.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
