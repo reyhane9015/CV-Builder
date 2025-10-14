e38937a617c5458cd9e747fc458c6d87322685f9
reyhane9015 <rafati.reyhaneh@yahoo.com>
2025-10-14 12:27:37 +0330

    add vercel deploy

--- backend/server.js
+++ backend/server.js
@@ -10,21 +10,29 @@ const resumeRoutes = require("./routes/resumeRoutes");
 const app = express();
 
 // ✅ Allowed origins
-const allowedOrigins = [
-  "http://localhost:5173", // your local frontend
-  "https://cv-builder-78vq.vercel.app", // your Vercel deployed frontend
-];
+// const allowedOrigins = [
+//   "http://localhost:5173", // your local frontend
+//   "https://cv-builder-78vq.vercel.app", // your Vercel deployed frontend
+// ];
+
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
+    origin: process.env.CLIENT_URL || "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
